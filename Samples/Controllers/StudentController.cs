2c237096d2fec9ca332884f474ab0b3438bc18cb
pramukh swami <dgbmsm13@gmail.com>
2019-01-27 22:55:30 +0530

    Commited on 27/01

---
+++ Samples/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Samples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Samples.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(Student student)
+        {
+            String message = "";
+            bool Status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities11 dc = new MyDatabaseEntities11())
+                {
+                    dc.Students.Add(student);
+                    dc.SaveChanges();
+                    message = "Registration successfully done.";
+                    Status = true;
+                }
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(student);
+        }
+    }
+}
---
+++ Samples/Models/Extended/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Samples.Models
+{
+
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+        
+    }
+    public class StudentMetadata
+    {
+        public int StudentID { get; set; }
+        [Display (Name = "Name")]
+        [Required]
+        public string StudentName { get; set; }
+        [Required]
+        public Nullable<int> Age { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
